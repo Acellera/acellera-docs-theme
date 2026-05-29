fbd8a0bfb974399f5bff0f37337584dec5ab9d9c
Stefan Doerr <stefdoerr@gmail.com>
2026-05-29 17:55:27 +0300

    better default

--- acellera_docs_theme/molstar.py
+++ acellera_docs_theme/molstar.py
@@ -56,8 +56,8 @@ MAX_FORMAL_CHARGE_LABELS = 200
 _BALL_AND_STICK_SELECTORS = ("ligand", "ion", "water")
 
 # Ball-and-stick atom/bond thickness. MVS's size_factor default of 1 gives
-# oversized spheres; 0.3 yields a slim ball-and-stick close to moleculekit's.
-BALL_AND_STICK_SIZE_FACTOR = 0.3
+# oversized spheres; 0.5 yields a balanced ball-and-stick close to moleculekit's.
+BALL_AND_STICK_SIZE_FACTOR = 0.5
 
 # molstar's cartoon needs a standard polymer backbone trace. Structures with
 # fewer cartoon-able standard residues than this (small molecules, ligands,
